06ed5d0f9154d04a8a505b1e48ff7d79e8479287
Wyatt Radkiewicz <wyattwradkiewicz@gmail.com>
2025-02-19 13:03:52 -0700

    Added conversion, minimization is all that's left

--- src/dfa.zig
+++ src/dfa.zig
@@ -9,6 +9,24 @@ pub const State = struct {
 
     /// non-null token represents accepting state
     token: ?usize = null,
+
+    pub fn format(
+        self: State,
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        _ = fmt;
+        _ = options;
+
+        try writer.print("State{{ .trans = {{ ", .{});
+        for (self.trans, 0..) |t, i| {
+            if (t) |trans| {
+                try writer.print("'{c}' -> {}", .{ @as(u8, @truncate(i)), trans });
+            }
+        }
+        try writer.print(" }}, token = {?} }} }}", .{self.token});
+    }
 };
 
 /// When converting an nfa to a dfa, you use a set to track each of the new states
@@ -16,83 +34,53 @@ const Set = struct {
     nfa_states: []const usize,
     dfa_state: State,
 
-    /// Find lambda neighbors of the state and construct a set from that
-    fn init(fa: []const nfa.State, from: usize, on: ?u8) Set {
-        const neighbors = nfa.Neighbors.find(fa, from, on);
-        return .{
-            .nfa_states = neighbors.states,
-            .dfa_state = .{ .token = neighbors.token },
-        };
+    /// Find neighbors of the state and construct a set from that
+    fn init(fa: []const nfa.State, from: []const usize, on: ?u8) Set {
+        // Visit every state's neighbors from the list
+        var n = nfa.Neighbors.init(fa, on);
+        for (from) |state| {
+            n.visit(state, false);
+        }
+        const states = n.neighbors();
+
+        // Return the first seen final token, (they should all be the same)
+        // TODO: (add sanity check maybe?)
+        return for (states) |state| {
+            if (fa[state].token) |token| {
+                break .{
+                    .nfa_states = states,
+                    .dfa_state = .{ .token = token },
+                };
+            }
+        } else .{ .nfa_states = states, .dfa_state = .{} };
     }
 
-    /// Create a new set from set edges, with a dfa_state only containing the common token.
-    /// If there is no common token, then an `@compileError` will be thrown
-    fn combine(sets: []const Set) Set {
-        var full_len: usize = 0;
-        var token: ?usize = null;
-        for (sets) |set| {
-            full_len += set.nfa_states.len;
-            if (set.dfa_state.token) |t| {
-                if (token == null or token == t) {
-                    token = t;
-                } else {
-                    @compileError("joining together multiple sets in nfa->dfa conversion!");
-                }
+    pub fn format(
+        self: Set,
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        _ = fmt;
+        _ = options;
+
+        try writer.print("Set{{ nfa_states = {any}, dfa_state = State{{ trans = .{{ ", .{self.nfa_states});
+        for (self.dfa_state.trans, 0..) |t, i| {
+            if (t) |trans| {
+                try writer.print("'{c}' -> {}", .{ i, trans });
             }
         }
-
-        const states = get_full_states: {
-            // All the states ordered, without repeating entries
-            var states = std.BoundedArray(usize, full_len).init(0) catch unreachable;
-
-            // Keep adding the states until there are no more to add
-            var completed_sets = 0;
-            var idxs = [1]usize{0} ** sets.len;
-            while (completed_sets != sets.len) {
-                // Get the min state id
-                var min: usize = std.math.maxInt(usize);
-                for (sets, idxs) |set, idx| {
-                    if (set.nfa_states[idx] < min) min = set.nfa_states[idx];
-                }
-
-                // Advance all states that have this state id (since they are ordered and don't have
-                // repeating entries, we only need to check this once)
-                completed_sets = 0;
-                for (sets, &idxs) |set, *idx| {
-                    if (set.nfa_states[idx.*] == min) idx.* += 1;
-                }
-
-                // Add the state to the full list
-                states.append(min) catch unreachable;
-            }
-
-            break :get_full_states @as([states.len]usize, states.slice()[0..].*);
-        };
-        
-        return .{
-            .dfa_state = .{ .token = token },
-            .nfa_states = states,
-        };
+        try writer.print("}}, token = {?} }} }}", .{self.dfa_state.token});
     }
 };
-
-const Dfa = std.BoundedArray(Set, 1024);
-
-/// Tries to add the set to the dfa, and if it's already there, returns the pointer of the current
-/// one instead.
-fn addSet(dfa: *Dfa, set: Set) usize {
-    for (dfa.slice(), 0..) |i, idx| {
-        if (std.mem.eql(usize, i.nfa_states, set.nfa_states)) return idx;
-    }
-    dfa.append(set) catch @panic("dfa conversion set overflow!");
-    return dfa.len - 1;
-}
 
 /// Returns a transition table with the first entry corresponding to the starting state
 pub fn convert(fa: []const nfa.State) []const State {
     // Create starting state
-    var dfa = Dfa.init(0) catch unreachable;
-    _ = addSet(dfa, Set.init(fa, 0));
+    var dfa = std.BoundedArray(Set, 1024).init(0) catch unreachable;
+    dfa.append(Set.init(fa, &.{0}, null)) catch unreachable;
+
+    @setEvalBranchQuota(fa.len * fa.len * 1000 * 1000);
 
     // Start finding neighbors as long as there are more
     var current: usize = 0;
@@ -100,23 +88,58 @@ pub fn convert(fa: []const nfa.State) []const State {
         // Find neighbors for every transition
         const state = &dfa.buffer[current];
         for (0..256) |byte| {
-            var sets: [state.*.nfa_states.len]Set = undefined;
-            for (state.*.nfa_states, 0..) |nfa_state, idx| {
-                sets[idx] = Set.init(fa, nfa_state, byte);
-            }
+            const set = Set.init(fa, state.*.nfa_states, byte);
 
-            const combined = Set.combine(&sets);
-            state.dfa_state.trans[byte] = addSet(combined);
+            // This represents no transitions, so don't add the transition (or an empty set)
+            if (set.nfa_states.len == 0) continue;
+
+            // Set the transition to what the set's dfa index is
+            state.*.dfa_state.trans[byte] = put_set: {
+                // Try to find the state and return it's index
+                for (dfa.slice(), 0..) |i, idx| {
+                    if (std.mem.eql(usize, i.nfa_states, set.nfa_states)) {
+                        break :put_set idx;
+                    }
+                }
+
+                // Add a new state to do
+                dfa.append(set) catch @panic("dfa conversion set overflow!");
+                break :put_set dfa.len - 1;
+            };
         }
     }
-    
+
+    // Convert the sets to an actual dfa
     var dfa_states: [dfa.len]State = undefined;
-    for (dfa.slice(), 0..) |set, i| {
-        dfa_states[i] = set.dfa_state;
+    for (dfa.slice(), &dfa_states) |set, *state| {
+        state.* = set.dfa_state;
     }
     const final_dfa = dfa_states;
     return &final_dfa;
 }
 
+/// Helper function to generate states
+fn testState(trans: []const struct { u8, usize }, token: ?usize) State {
+    var state = State{ .token = token };
+    inline for (trans) |t| {
+        state.trans[t[0]] = t[1];
+    }
+    return state;
+}
+
 test "nfa to dfa conversion" {
+    try std.testing.expectEqualSlices(State, comptime &.{
+        testState(&.{.{ 'a', 1 }}, null),
+        testState(&.{.{ 'b', 2 }}, null),
+        testState(&.{.{ 'c', 3 }}, null),
+        testState(&.{}, 0),
+    }, comptime convert(nfa.construct(parser.parse("abc").ok, 0)));
+    try std.testing.expectEqualSlices(State, comptime &.{
+        testState(&.{.{ 'a', 1 }}, null),
+        testState(&.{.{ 'a', 1 }}, 0),
+    }, comptime convert(nfa.construct(parser.parse("a+").ok, 0)));
+    try std.testing.expectEqualSlices(State, comptime &.{
+        testState(&.{ .{ 'a', 1 }, .{ 'b', 1 } }, null),
+        testState(&.{}, 0),
+    }, comptime convert(nfa.construct(parser.parse("a|b").ok, 0)));
 }
